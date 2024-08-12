562e3cf41cc53ee31887d678c12783d1efa4bc7b
zjl <zhujialai@icloud.com>
2024-08-12 10:34:10 +0800

    root path

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+
 	httpsrv "zen108.com/lspsrv/pkg"
 )
 
-
 func main() {
-	r :=  httpsrv.NewRouter("")
+	root := flag.String("root", "", "root path")
+	flag.Parse()
+	r := httpsrv.NewRouter(*root)
 	// http.HandleFunc("/path", helloWorld) // 注册路由处理函数
 	fmt.Println("Server listening on :18080")
 	if err := http.ListenAndServe(":18080", r); err != nil {
 		panic(err)
 	}
 }
-
