c7f05d5009520a2d4112eed1326a92c36fe48422
wellcomez <zhu.jialai@gmail.com>
2024-08-12 16:30:01 +0800

    add build yarn option

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
-yarn build
+build_web=false
+while getopts "w" opt; do
+  case $opt in
+  w)
+    build_web=true
+    ;;
+  esac
+done
+
+if [[ $build_web == true ]]; then
+  yarn build
+fi
+
 pushd build || exit
 go-bindata -o ../bindata/bindata.go -pkg bindata *
 popd || exit
